eaebee20120b7527eb9e903ef96bf94424babae8
Brian Beck <exogen@gmail.com>
2010-02-26 03:25:03 -0500

    Experimental test runner for use with Django.

---
+++ noseachievements/djangotest.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import unittest
+from optparse import OptionParser
+
+from nose.config import Config
+from django.test.simple import DjangoTestRunner, DjangoTestSuiteRunner
+
+from noseachievements.plugin import AchievementsPlugin
+from noseachievements.result import TextTestResult
+
+
+class DjangoTestRunner(DjangoTestRunner):
+    result_class = TextTestResult
+
+    def __init__(self, *args, **kwargs):
+        super(DjangoTestRunner, self).__init__(*args, **kwargs)
+        self.plugin = AchievementsPlugin()
+        parser = OptionParser()
+        self.plugin.options(parser, os.environ)
+        options, args = parser.parse_args()
+        self.plugin.configure(options, Config())
+        self.plugin.enabled = True
+
+    def run(self, *args, **kwargs):
+        self.plugin.begin()
+        result = super(DjangoTestRunner, self).run(*args, **kwargs)
+        self.plugin.setOutputStream(sys.stdout)
+        self.plugin.finalize(result)
+        return result
+
+    def _makeResult(self):
+        # Monkeypatch TextTestRunner to avoid copying & pasting code. :(
+        old_makeResult = unittest.TextTestRunner._makeResult
+        runner = self
+        def makeResult(self):
+            return runner.result_class(self.stream, self.descriptions,
+                                       self.verbosity, plugin=self.plugin)
+        unittest.TextTestRunner._makeResult = makeResult
+        result = super(DjangoTestRunner, self)._makeResult()
+        unittest.TextTestRunner._makeResult = old_makeResult
+        return result
+
+class DjangoTestSuiteRunner(DjangoTestSuiteRunner):
+    runner_class = DjangoTestRunner
+
+    def run_suite(self, suite, **kwargs):
+        return self.runner_class(verbosity=self.verbosity,
+                                 failfast=self.failfast).run(suite)
+
---
+++ noseachievements/result.py
@@ -0,0 +1,25 @@
+import unittest
+
+from nose.util import test_address
+
+
+class TextTestResult(unittest._TextTestResult):
+    def __init__(self, *args, **kwargs):
+        self.plugin = kwargs.pop('plugin', None)
+        unittest._TextTestResult.__init__(self, *args, **kwargs)
+
+    def addSuccess(self, test):
+        self.plugin.addSuccess(test)
+        self.plugin.afterTest(test)
+        return unittest._TextTestResult.addSuccess(self, test)
+
+    def addError(self, test, err):
+        self.plugin.addError(test)
+        self.plugin.afterTest(test)
+        return unittest._TextTestResult.addError(self, test, err)
+
+    def addFailure(self, test, err):
+        self.plugin.addFailure(test)
+        self.plugin.afterTest(test)
+        return unittest._TextTestResult.addFailure(self, test, err)
+
